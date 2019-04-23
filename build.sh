ea261e9f4c9cccff2b31930c7a6250a5a51cc19b
Moshe Golan <mosheg@mosheg>
2019-04-23 23:53:11 +0300

    cleanup dockerfile and build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+#!/bin/bash 
+mkdir -p bin build
+docker build -t ubuntu:gcc-8 . 
+docker run -v $(readlink -f .):/src -it ubuntu:gcc-8 bash -c 'cd /src; make clean; make; chmod 777 bin/* build/*'
 
-# docker build -t ubuntu:gcc-8 . 
-docker run -v $(readlink -f .):/src -it ubuntu:gcc-8
