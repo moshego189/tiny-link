c2785bec9e7a15c6e00c53cfdfe1630d37322e9f
Moshe Golan <mosheg@mosheg>
2019-04-24 00:24:46 +0300

    docker run fix --rm flag

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
 mkdir -p bin build
 docker build -t ubuntu:gcc-8 . 
-docker --rm run -v $(readlink -f .):/src -it ubuntu:gcc-8 bash -c 'cd /src; make clean; make; chmod 777 bin/* build/*'
+docker run --rm -v $(readlink -f .):/src -it ubuntu:gcc-8 bash -c 'cd /src; make clean; make; chmod 777 bin/* build/*'
 
